e331de2018b7c6bd61ca7164432d5d0c35b0b15c
Shah <meet29in@gmail.com>
2018-04-12 20:12:33 -0700

    Meet Shah: Lesson02 part2: generate grid; move redundant loop section to a helper method

--- students/meet_shah/lesson02/print_grid.py
+++ students/meet_shah/lesson02/print_grid.py
@@ -17,6 +17,10 @@ def print_grid(x):
             print(midline)
         print(topline)
  
+def help_print_line(startpattern, endpattern, repeat):
+    for i in range(0,repeat):
+        print(startpattern, end='')
+    print(endpattern)
 
 def print_grid2(x,y):
 
@@ -30,17 +34,29 @@ def print_grid2(x,y):
     midmid   = "  "
     midend   = "|"
 
-    pattern1 = topstart + topmid*y
+    pattern1 = topstart + topmid*grid_repeat
     pattern1end = topend
     
-    pattern2 = midstart + midmid*y
+    pattern2 = midstart + midmid*grid_repeat
     pattern2end = midend
 
-    for i in range(0,grid_repeat):
+    for i in range(0,box_repeat):
+        help_print_line(pattern1, pattern1end, box_repeat)
+#        for i in range(0,box_repeat):
+#            print(pattern1, end='')
+#        print(pattern1end)
+        for i in range(0,box_repeat):
+            for i in range(0,box_repeat):
+                print(pattern2, end='')
+            print(pattern2end)
+
+    for i in range(0,box_repeat):
         print(pattern1, end='')
+    print(pattern1end)
+
 
 print_grid2(3,4)
 print()
-print_grid2(10,10)
+print_grid2(5,10)
 print()
 
