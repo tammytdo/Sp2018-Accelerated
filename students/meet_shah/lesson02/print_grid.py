68358dd0b281bbf2fbf8c1529b360c23432aaa74
Shah <meet29in@gmail.com>
2018-04-12 20:01:46 -0700

    Meet Shah: Lesson02 part2: initial testing with reduced redundancy to generate top box

--- students/meet_shah/lesson02/print_grid.py
+++ students/meet_shah/lesson02/print_grid.py
@@ -18,9 +18,29 @@ def print_grid(x):
         print(topline)
  
 
-print_grid(3)
-print_grid(4)
-print_grid(5)
-print_grid(6)
-print_grid(15)
+def print_grid2(x,y):
+
+    box_repeat = x
+    grid_repeat = y 
+    topstart = "+ "
+    topmid   = "- "
+    topend   = "+"
+
+    midstart = "| "
+    midmid   = "  "
+    midend   = "|"
+
+    pattern1 = topstart + topmid*y
+    pattern1end = topend
+    
+    pattern2 = midstart + midmid*y
+    pattern2end = midend
+
+    for i in range(0,grid_repeat):
+        print(pattern1, end='')
+
+print_grid2(3,4)
+print()
+print_grid2(10,10)
+print()
 
