ce1f42c28a1d3cc15328cbe6971535668a7b3526
Bri Dean <Briadean@protonmail.com>
2018-04-13 13:48:02 -0700

    Completed break_me and fizzbuzz exercises, added psuedocode for grid_printer and series exercises.

---
+++ students/Briadean/lesson02/grid_printer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""Print grids based on increasingly complex and scalable required parameters"""
+def grid_printer_1():
+    pass
+
+def grid_printer_2(size_num):
+    pass
+
+def grid_printer_3(row_num, column_num):
+    pass
+
+
+if __name__ == "__main__":
---
+++ students/Briadean/lesson02/series.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+def fibonacci(n):
+    """A function to compute the Fibonacci series and return the nth value of the series """
+    pass
+
+
+def lucas(n):
+    """A function to compute the Lucas series and return the nth value of the series"""
+    pass
+
+
+def sum_series():
+    """Return the nth value of the Fibonnaci series when given no optional arguments"""
+    # Insert code
+
+    """Return the nth value of the Lucas series when given optional arguments 2 and 1"""
+    # Insert code
+
+    """Return the nth value of a general series given using any other optional arguments as the first two integers in
+    the sequence"""
+    # Insert code
+    pass
+
+# Assert statements for testing, each with own comment explaining function
+
+
+if __name__ == "__main__":
