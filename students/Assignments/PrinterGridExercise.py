976e8a2fd2b8a42d05c8b2e0c95368de047fa7e9
Wayne Olson <weepler@D-108-179-176-113.dhcp4.washington.edu>
2018-04-12 20:51:24 -0700

    for loop

--- students/Assignments/PrinterGridExercise.py
+++ students/Assignments/PrinterGridExercise.py
@@ -1,4 +1,4 @@
-#Upload to GIT Test
+#print grid with single variable
 
 plus = '+'
 
@@ -8,16 +8,33 @@ line = '|'
 
 space = ' '
 
+#rows occur for 1, (n-1)/2, or when n = n (i.e. the last row)
+
+#    for (n == 1 or n == yolo or n = n)
+#        print(row)
+#    elif
+#        print(column)
+    #for n in range (1,n):
+        #if n ==[1,yolo,n]:  
+            #print(row)
+    #else: 
+        #print(column)
+
 
 def print_grid(n):
     yolo = ((n-1)//2)
     row = (plus+minus*yolo+plus+minus*yolo+plus)
     column = (line+space*yolo+line+space*yolo+line)
-    print(row)
-    print(column)
-    print(row)
-    print(column)
-    print(row,end='')
+    
+    while n > 0:
+        if n ==1 or n == yolo or n ==15:
+            print(row)
+        n = n - 1
+    print ('done') 
+
 
 print_grid(3)
+    
+
+
 
