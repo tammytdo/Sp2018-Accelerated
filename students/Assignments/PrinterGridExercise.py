94a3141332118a7cdc85a373acc5dc0cb7ae1959
Wayne Olson <weepler@D-108-179-176-113.dhcp4.washington.edu>
2018-04-12 19:59:35 -0700

    working on printer function

--- students/Assignments/PrinterGridExercise.py
+++ students/Assignments/PrinterGridExercise.py
@@ -1,1 +1,23 @@
 #Upload to GIT Test
+
+plus = '+'
+
+minus = '-'
+
+line = '|'
+
+space = ' '
+
+
+def print_grid(n):
+    yolo = ((n-1)//2)
+    row = (plus+minus*yolo+plus+minus*yolo+plus)
+    column = (line+space*yolo+line+space*yolo+line)
+    print(row)
+    print(column)
+    print(row)
+    print(column)
+    print(row,end='')
+
+print_grid(3)
+
