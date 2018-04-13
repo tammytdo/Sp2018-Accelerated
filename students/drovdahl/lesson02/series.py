c755fd2415b4d537905b572e50056464d484233f
Ryan Drovdahl <rdrovdahl@D-108-179-189-18.dhcp4.washington.edu>
2018-04-12 20:20:14 -0700

    rdrovdahl lesson 2 series.py - added basic recursive formula to functions

--- students/drovdahl/lesson02/series.py
+++ students/drovdahl/lesson02/series.py
@@ -5,12 +5,29 @@ def fibonacci(n):
     '''
     This function will return the 'n'th value in the fibonacci series
     '''
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 def lucas(n):
     '''
     This function returns the 'n'th value in the lucas series
     '''
+    if n == 0:
+        return 2
+    if n == 1:
+        return 1
+    else:
+        return lucas(n-1) + lucas(n-2)
 
 
-def sum_series()
+def sum_series(n, a=0, b=1):
+    '''
+    This function determins which element in the series to print.
+    The two optional parameters will have default values of 0 and 1 and
+    will determine the first two values for the series to be produced.
+    '''
