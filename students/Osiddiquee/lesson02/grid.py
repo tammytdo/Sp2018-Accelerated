c803ec131164a1ee89b91aa2d5cdf2e1ca225cd3
Siddiquee <n0291178@lm.lmig.com>
2018-04-12 20:25:24 -0700

    finished grid.py

---
+++ students/Osiddiquee/lesson02/grid.py
@@ -0,0 +1,15 @@
+border = '+' + 4 * ' -' + ' +' + 4 * ' -' + ' +'
+interior = '|' + 9 * ' ' + '|' + 9 * ' ' + '|'
+
+
+print(border)
+print(interior)
+print(interior)
+print(interior)
+print(interior)
+print(border)
+print(interior)
+print(interior)
+print(interior)
+print(interior)
+print(border)
