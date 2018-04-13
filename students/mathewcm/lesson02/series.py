1ecca618b4dc7c4a6b026c7afd8cde0826cc45ba
mathewcmartin <chactasstryker@gmail.com>
2018-04-12 23:33:50 -0700

    Added lucas sequence to /mathewcm/lesson02/series.py for Mathew C Martin

--- students/mathewcm/lesson02/series.py
+++ students/mathewcm/lesson02/series.py
@@ -16,4 +16,24 @@ def fibonacci(n):
         series.append(c)
         a = b
         b = c
-    return series[-3]
+    return series[-4]
+
+def lucas(n):
+    """
+    This function will return the nth iteration of that value in the Lucas sequence.
+    """
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    # n > 2
+    a = 1
+    b = 3
+    # First two members of the series
+    series = [a, b]
+    for i in range(n):
+        c = a + b
+        series.append(c)
+        a = b
+        b = c
+    return series[-4]
