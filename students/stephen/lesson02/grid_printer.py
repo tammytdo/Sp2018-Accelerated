2ea2a587b4bb266bd0db1fb48b36d092a711a087
Stephen <ssoukasene@gmail.com>
2018-04-12 20:31:06 -0700

    First attempt at part 2 of grid printer exercise.

--- students/stephen/lesson02/grid_printer.py
+++ students/stephen/lesson02/grid_printer.py
@@ -56,3 +56,54 @@ def print_test():
 
 
 print_test()
+
+# Part 2
+# Create a function print_grid() such that
+# print_grid(3) would print a smaller grid:
+# + - + - +
+# |   |   |
+# + - + - +
+# |   |   |
+# + - + - +
+# print_grid(15) prints a larger grid:
+# + - - - - - - - + - - - - - - - +
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# + - - - - - - - + - - - - - - - +
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# |               |               |
+# + - - - - - - - + - - - - - - - +
+
+
+def print_grid(n):
+    p = '+'
+    m = '-'
+    s = ' '
+    c = '|'
+    side = p + s + (n // 2) * (m + s)
+    row = (2 * side) + p
+    col = c + (len(side) - 1) * s
+    column = (2 * col) + c
+    print(row)
+    for i in range(0, n // 2):
+        print(column)
+    print(row)
+    for i in range(0, n // 2):
+        print(column)
+    print(row)
+    return
+
+
+print_grid(3)
+
+print_grid(15)
