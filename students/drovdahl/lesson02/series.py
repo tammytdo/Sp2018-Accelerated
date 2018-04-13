b75530b20c4050427b66dd87023d45ff79a59784
Ryan Drovdahl <rdrovdahl@D-108-179-189-18.dhcp4.washington.edu>
2018-04-12 19:56:56 -0700

    rdrovdahl lesson 2 series.py initial file commit

---
+++ students/drovdahl/lesson02/series.py
@@ -0,0 +1,16 @@
+#! /usr/local/bin/python3
+
+
+def fibonacci(n):
+    '''
+    This function will return the 'n'th value in the fibonacci series
+    '''
+
+
+def lucas(n):
+    '''
+    This function returns the 'n'th value in the lucas series
+    '''
+
+
+def sum_series()
