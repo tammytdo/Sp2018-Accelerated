f5d415043908099383abdcd58ce464589c3eeb34
mathewcmartin <chactasstryker@gmail.com>
2018-04-12 23:13:00 -0700

    Committing fibonacci excercise in series.py Lesson02 complete. Based my code on Amit Saha fibonacci list p.59 Doing Math with Python.

---
+++ students/mathewcm/lesson02/series.py
@@ -0,0 +1,19 @@
+def fibonacci(n):
+    """
+    This function will return the nth iteration of that value in the fibonacci sequence.
+    """
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    # n > 2
+    a = 1
+    b = 1
+    # First two members of the series
+    series = [a, b]
+    for i in range(n):
+        c = a + b
+        series.append(c)
+        a = b
+        b = c
+    return series[-3]
