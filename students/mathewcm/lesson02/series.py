cb90fcdef56e0e01d93ec911eccda01f25169460
mathewcmartin <chactasstryker@gmail.com>
2018-04-14 12:08:10 -0700

    Committing series.py changes including fibonacci(), lucas() and sum_series() functions for Lesson02 fibonacci exercise.

--- students/mathewcm/lesson02/series.py
+++ students/mathewcm/lesson02/series.py
@@ -38,7 +38,7 @@ def lucas(n):
         b = c
     return series[-4]
 
-def sum_series(n, a, b):
+def sum_series(n, a=0, b=1):
     """
     This function is a generalization of the fibonacci sequence and the Lucas sequence and defaults to the values of the fibonacci sequence if no optional inputs for a and b are entered. The function returns the nth value in the series.
     """
@@ -46,11 +46,6 @@ def sum_series(n, a, b):
         return [1]
     if n == 2:
         return [1, 1]
-    # n > 2
-    if a is None:
-        return a == 0
-    if b is None:
-        return b == 1
     # First two members of the series
     series = [a, b]
     for i in range(n):
@@ -58,4 +53,4 @@ def sum_series(n, a, b):
         series.append(c)
         a = b
         b = c
-    return series[-4]
+    return series[-3]
