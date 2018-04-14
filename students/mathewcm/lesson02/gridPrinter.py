bb4ea649b62bbd415e654bedbba0d52485384bba
mathewcmartin <chactasstryker@gmail.com>
2018-04-14 12:36:25 -0700

    Committing first excercise of Lesson02 Grid Printer for student/mathewcm

---
+++ students/mathewcm/lesson02/gridPrinter.py
@@ -0,0 +1,18 @@
+def beam():
+    print("+----+----+")
+
+def post():
+    print("|    |    |")
+
+def printGrid():
+    beam()
+    post()
+    post()
+    post()
+    post()
+    beam()
+    post()
+    post()
+    post()
+    post()
+    beam()
