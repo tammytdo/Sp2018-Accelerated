660dd2e34a24e49fee89e28458b7a232b6a13786
mathewcmartin <chactasstryker@gmail.com>
2018-04-15 15:47:41 -0700

    Committing completed fizzbuzz.py fizz buzz excercise Lesson02 UW Python Accellerate /students/mathewcm. Woohoogit add .!

---
+++ students/mathewcm/lesson02/fizzbuzz.py
@@ -0,0 +1,14 @@
+#Fizzbuzz exercise Lesson02 UW Python Accelerated
+
+def fizzbuzz():
+    for i in range(100):
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizzbuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
