5b1ef2b5bd2b0893582c2eb52d195ac8f8b33dee
mathewcmartin <chactasstryker@gmail.com>
2018-04-15 15:54:09 -0700

    Expanded range to 101 to include 100. Lesson02/mathewcm fizzbuzz.py

--- students/mathewcm/lesson02/fizzbuzz.py
+++ students/mathewcm/lesson02/fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     """
     fizzbuzz() function prints a range of numbers from 1 to 100 with all values that are a factor of 3 replaced with the word Fizz and all the values that are a factor of 5 replaced with the word Buzz. Except those numbers that are a factor of both; they are replaced with the word Fizzbuzz.
     """
-    for i in range(100):
+    for i in range(101):
         if i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz")
         elif i % 3 == 0:
