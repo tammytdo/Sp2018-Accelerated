30a1b3cdc5e0fab8ab01740c12bd7211e4fe50db
mathewcmartin <chactasstryker@gmail.com>
2018-04-15 15:51:26 -0700

    Added docstring to fizzbuzz.py

--- students/mathewcm/lesson02/fizzbuzz.py
+++ students/mathewcm/lesson02/fizzbuzz.py
@@ -1,6 +1,9 @@
 #Fizzbuzz exercise Lesson02 UW Python Accelerated
 
 def fizzbuzz():
+    """
+    fizzbuzz() function prints a range of numbers from 1 to 100 with all values that are a factor of 3 replaced with the word Fizz and all the values that are a factor of 5 replaced with the word Buzz. Except those numbers that are a factor of both; they are replaced with the word Fizzbuzz.
+    """
     for i in range(100):
         if i % 3 == 0 and i % 5 == 0:
             print("Fizzbuzz")
