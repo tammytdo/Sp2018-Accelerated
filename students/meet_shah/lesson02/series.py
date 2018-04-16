229436ed9002c63100bf9dbe842a61f6e515c2f0
Meet Shah <meetshah@Meets-MacBook-Pro.local>
2018-04-15 23:43:57 -0700

    Meet Shah: Lesson 2 exercise 3: added lucas(n)

--- students/meet_shah/lesson02/series.py
+++ students/meet_shah/lesson02/series.py
@@ -12,3 +12,21 @@ print(fib(10))
 print(fib(1))
 print(fib(2))
 print(fib(3))
+
+
+
+def lucas(n):
+
+  if n == 0:
+    return 2
+  if n == 1: 
+    return 1
+
+  return lucas(n-1) + lucas(n-2)
+
+
+print(lucas(0))
+print(lucas(1))
+print(lucas(2))
+print(lucas(5))
+
