a1f2cbae2d2bd1a28128b0023594f54984926a04
Bri Dean <Briadean@protonmail.com>
2018-04-15 22:03:55 -0700

    Finished Fibonacci Series exercise/module

--- students/Briadean/lesson02/series.py
+++ students/Briadean/lesson02/series.py
@@ -2,27 +2,43 @@
 
 def fibonacci(n):
     """A function to compute the Fibonacci series and return the nth value of the series """
-    pass
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 def lucas(n):
     """A function to compute the Lucas series and return the nth value of the series"""
-    pass
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return lucas(n - 1) + lucas(n - 2)
 
 
-def sum_series():
-    """Return the nth value of the Fibonnaci series when given no optional arguments"""
-    # Insert code
+def sum_series(n, a=0, b=1):
+    """A generalized function to return the nth value of the Fibonnaci series by default, the lucas series when
+    given the optional parameters a=2, b=1 and the nth value of any series based on this formula when given any
+    other optional parameters."""
+    if n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        return sum_series(n - 1, a, b) + sum_series(n - 2, a, b)
 
-    """Return the nth value of the Lucas series when given optional arguments 2 and 1"""
-    # Insert code
 
-    """Return the nth value of a general series given using any other optional arguments as the first two integers in
-    the sequence"""
-    # Insert code
-    pass
-
-# Assert statements for testing, each with own comment explaining function
-
+# Assert statement to demonstrate correct fibonacci series values are returned by the given "nth" parameter
+assert sum_series(4) == 3
+# Assert statement to demonstrate correct lucas series values are returned by the given "nth" parameter
+assert sum_series(9, a=2, b=1) == 76
 
 if __name__ == "__main__":
+
+    print(fibonacci(9))
+    print(lucas(4))
+    print(sum_series(4, a=4, b=2))
