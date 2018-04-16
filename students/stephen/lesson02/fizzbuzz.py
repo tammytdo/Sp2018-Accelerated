3ccc4262eca3a6ad5524745510c04183c6ea34e1
Stephen <ssoukasene@gmail.com>
2018-04-15 22:26:17 -0700

    First attempt at Fizz Buzz problem.

---
+++ students/stephen/lesson02/fizzbuzz.py
@@ -0,0 +1,20 @@
+# The Classic Fizz Buzz Problem
+# Write a program that prints the numbers from 1 to 100 inclusive.
+# But for multiples of three print “Fizz” instead of the number.
+# For the multiples of five print “Buzz” instead of the number.
+# For numbers which are multiples of both three and five print “FizzBuzz” instead.
+
+def fizzbuzz():
+    for i in range(0,100):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        elif i % 5 == 0:
+            print('Buzz')
+        else:
+            print(i)
+    return
+
+
+fizzbuzz()
