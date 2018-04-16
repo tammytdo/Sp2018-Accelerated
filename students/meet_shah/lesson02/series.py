211734fef2484a29890d2fb815a2ff4a8aa0f8c9
Meet Shah <meetshah@Meets-MacBook-Pro.local>
2018-04-15 23:40:31 -0700

    Meet Shah: Lesson 2 exercise 3: fixing the recurssion; fib(1) and fib(2) now return 1

--- students/meet_shah/lesson02/series.py
+++ students/meet_shah/lesson02/series.py
@@ -3,9 +3,12 @@ def fib(n):
 
   if n == 0:
     return 0
-  return n + fib(n-1)
+  if n == 1:
+    return 1
+  return fib(n-1) + fib(n-2)
 
 
 print(fib(10))
 print(fib(1))
 print(fib(2))
+print(fib(3))
