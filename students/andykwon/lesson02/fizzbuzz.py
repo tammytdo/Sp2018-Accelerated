0fa1d1ffe1226721e6c2d5d14de9e13ce5cf08b0
Andy <kwona714@hotmail.com>
2018-04-15 18:45:55 -0700

    Andy Kwon FizzBuzz Commit

---
+++ students/andykwon/lesson02/fizzbuzz.py
@@ -0,0 +1,16 @@
+def fizz_buzz (num):
+
+    inclusive_num = num + 1
+
+    for i in range (inclusive_num):
+
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+
+fizz_buzz(100)
