b870eb6c6411f8341043e59e8767b7a8dca5a38d
Wayne Olson <weepler@D-173-250-207-164.dhcp4.washington.edu>
2018-04-16 20:50:26 -0700

    started mailroom and added while loop for the menu

---
+++ students/Wayne/Assignments/Lesson3/Mailroom.py
@@ -0,0 +1,45 @@
+"""
+Simple mailroom program for a non profit 
+"""
+
+#Start with interactive loop 
+
+import sys
+
+def thankyou():
+    print("This is the thank you\n")
+
+def report():
+    print ("This is the report function\n")
+
+def quit():
+    sys.exit()
+
+def mainloop():
+    print("Welcome to Mailroom")
+
+    while True:
+        print("What do you want to do?")
+        print("1)Thank You\n"
+              "2)Report\n"
+              "3)Quit\n")
+        response = input('>>')
+
+        print("response was:", response)
+        if response not in ("1","2","3"):
+            print("Not a Valid Response, input 1, 2, or 3***\n")
+            continue
+        elif response == "1":
+            thank_you()
+        elif response == "2":
+            report()
+        elif response == "3":
+            quit()
+            
+
+
+
+if __name__ == "__main__":
+    mainloop()
+
+
