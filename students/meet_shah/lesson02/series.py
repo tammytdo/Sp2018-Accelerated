f5508c76d77039872f86bb63bdfb848090286d67
Shah <meet29in@gmail.com>
2018-04-16 19:07:00 -0700

    Meet Shah: Lesson02 part3: now generalizing fib and lucas and moving sum_series up so generalized functions run

--- students/meet_shah/lesson02/series.py
+++ students/meet_shah/lesson02/series.py
@@ -1,11 +1,14 @@
+def sum_series(n, input1=0, input2=1):
+    if n == 0:
+        return input1
+    if n == 1:
+        return input2
+
+    return sum_series(n-1, input1, input2) + sum_series(n-2, input1, input2)
+
 
 def fib(n):
-
-  if n == 0:
-    return 0
-  if n == 1:
-    return 1
-  return fib(n-1) + fib(n-2)
+  return sum_series(n, 0, 1)
 
 
 print(fib(10))
@@ -18,13 +21,7 @@ print(fib(5))
 
 def lucas(n):
 
-  if n == 0:
-    return 2
-  if n == 1:
-    return 1
-
-  return lucas(n-1) + lucas(n-2)
-
+ return sum_series(n, 2, 1)
 
 print(lucas(0))
 print(lucas(1))
@@ -32,14 +29,6 @@ print(lucas(2))
 print(lucas(5))
 print("Print lucas(10)")
 print(lucas(10))
-
-def sum_series(n, input1=0, input2=1):
-    if n == 0:
-        return input1
-    if n == 1:
-        return input2
-
-    return sum_series(n-1, input1, input2) + sum_series(n-2, input1, input2)
 
 
 print("Output same value as lucas(10)")
