a47091055e175499f79b85fa371d5e4eb1901bfa
Ryan Drovdahl <rdrovdahl@Ryans-MacBook-Pro.local>
2018-04-19 01:04:27 -0700

    mailroom.py - completed the thankyou function

--- students/drovdahl/lesson03/mailroom.py
+++ students/drovdahl/lesson03/mailroom.py
@@ -20,7 +20,51 @@ donors = [('Iron Man', [1000000, 5000000, 10000000]),
 
 
 def thankyou():
-    print('this is the thankyou function\n')
+    global donors
+    print('So, you just recieved a new donation.  That\'s great!')
+    print('Let\'s record the donation and draft a THANK YOU message\n')
+    print('Who is the donor? Type the name of the donor or "list" to see a'
+          'list of existing donors.\n(type "quit" at any time to return to the'
+          'main menu)')
+    donor = input('>> ')
+    if donor.lower() == 'quit':
+        return
+    while donor.lower() == 'list':
+        os.system('clear')
+        print('Here\'s a list of our generous donors:\n  ', end='')
+        print(*[x[0] for x in donors], sep='\n  ')
+        print('Type the name of an existing or new donor.\n'
+              '(type "quit" at any time to return to the main menu)')
+        donor = input('>> ')
+        if donor.lower() == 'quit':
+            return
+    donor_donation = input('What is the donation amount? \n >> ')
+    donor_donation = donor_donation.replace('$', '').replace(',', '')
+    donor_donation = float(donor_donation)
+#    print('%.2f' % donor_donation)
+    x = 0
+    name_match = False
+    for name in donors:
+        if name[0] == donor:
+            donors[x][1].append(donor_donation)
+            name_match = True
+        x += 1
+    if name_match is False:
+        donors.append((donor, [donor_donation]))
+# compose a THANK YOU email
+    os.system('clear')
+    k = (donor_donation/5 * 2)
+    print('Below is an email tailored to this donor...\n\n')
+    print(f'''Dear {donor},
+        We at the Avengers Fund-a-Kitten Initiative would like to thank you for
+    your generous donation of $%.2f.\n
+    Taking advantage of our kitten matching partner, with these added funds we
+    will be able to provide {k} kitten(s) to well deserving little girls all
+    over the world including hard to reach places like Antacrtica and
+    Tacoma, WA!\n\n
+        Sincerely,
+        Your Friends at AFAK
+    ''' % donor_donation)
 
 
 def report():
@@ -33,7 +77,7 @@ def quit():
 
 def mainloop():
     os.system('clear')
-    print('''Avengers - Fund a kitten initiative
+    print('''Avengers: Fund-a-Kitten Initiative
   Because every little girl
   Everywhere in the world
   ...deserves a kitten
