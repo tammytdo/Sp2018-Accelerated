89c11142d2623fccc81ff905a63cdcd89172d17a
Shah <meet29in@gmail.com>
2018-04-19 16:34:38 -0700

    Previous method didn't work for all sequences re-doing based on what we learned in the class

--- students/meet_shah/lesson03/slicing.py
+++ students/meet_shah/lesson03/slicing.py
@@ -2,14 +2,13 @@ a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
 
 def exchange_first_last(seq):
-    mylist = list(seq)
-    mylist[0],mylist[len(mylist)-1] = mylist[len(mylist)-1], mylist[0]
-    if type(seq) == str:
-        seq = ''.join(mylist)
-    if type(seq) == tuple:
-        seq = tuple(mylist)
-    return seq
+   first = seq[0]
+   middle = seq[1:len(seq)-1]
+   last = seq[-1]
+   newseq = last + middle + first
+   return newseq
 
+print(exchange_first_last(a_string))
 
 assert exchange_first_last(a_string) == "ghis is a strint"
 assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
