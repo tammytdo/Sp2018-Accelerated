f1406fbf52dcbf1f8ba1fb85662451435cea98de
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-04-18 22:06:22 -0700

    created session 3 assignment documents with notes

---
+++ students/Wayne/Assignments/Lesson3/SlicingLabExercise.py
@@ -0,0 +1,16 @@
+# Slicing Lab Exercise
+
+# with the first and last items exchanged.
+
+
+# with every other item removed.
+
+
+# with the first and last 4 items removed, and every other item in between.
+
+
+# with the elements reversed (just with slicing).
+
+
+# with the middle third, then last third, then the first third
+# in the new order.
