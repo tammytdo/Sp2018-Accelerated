6a5d2380cb1a87ec30a934c0a365e28ac0ba2129
Shah <meet29in@gmail.com>
2018-04-19 16:51:21 -0700

    Works for strings

--- students/meet_shah/lesson03/slicing.py
+++ students/meet_shah/lesson03/slicing.py
@@ -1,15 +1,16 @@
 a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
-
+a_list = [3,4,5,6,7,8]
 def exchange_first_last(seq):
    first = seq[0]
    middle = seq[1:len(seq)-1]
    last = seq[-1]
-   newseq = last + middle + first
+   newseq = middle.append(first)
+   print(newseq)
    return newseq
 
-print(exchange_first_last(a_string))
-
-assert exchange_first_last(a_string) == "ghis is a strint"
+#print(exchange_first_last(a_tuple))
+print(exchange_first_last(a_list))
+#assert exchange_first_last(a_string) == "ghis is a strint"
 assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
 
