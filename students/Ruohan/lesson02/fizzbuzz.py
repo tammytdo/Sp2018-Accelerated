78d4e15cd78980ca40bdbeb22d958b426c07f7f8
Ruohan <tangtangruohan@hotmail.com>
2018-04-19 22:33:31 -0700

    work

--- students/Ruohan/lesson02/fizzbuzz.py
+++ students/Ruohan/lesson02/fizzbuzz.py
@@ -1,9 +1,21 @@
 for i in range(1, 101):
-    if i%3==0 and i%5==0:
+    if i % 3==0 and i % 5==0:
         print('FizzBuzz')
-    elif i%3==0:
+    elif i % 3==0:
         print('Fizz')
-    elif i%5==0:
+    elif i % 5==0:
         print('Buzz')
     else:
         print (i)
+
+
+for i in range(1, 101):
+    s = ''
+    if i % 3==0:
+        s = 'Fizz'
+    if i % 5==0:
+        s += 'Buzz'
+    if s == '':
+        s = str(i)
+
+    print (s)
--- students/Ruohan/lesson02/grid_printer_part2.py
+++ students/Ruohan/lesson02/grid_printer_part2.py
@@ -4,19 +4,15 @@ def grid(x):
     s1 = ' -'*y+' '
     s2 = '  '*y+' '
     line = point
-    line1 = ' '
     line2 = '|'
     for i in range(2):
-        line = line+s1+point
-        line1 = line1+s2+' '
-        line2 = line2+s2+'|'
+        line += s1+point
+        line2 += s2+'|'
 
     print (line)
     def graph(y):
         for i in range(y):
-            print (line1)
             print (line2)
-        print(line1)
         print(line)
     for i in range(2):
         graph(y)
