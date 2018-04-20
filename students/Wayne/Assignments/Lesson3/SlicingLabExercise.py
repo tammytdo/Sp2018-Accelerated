c1433033e3f186ccf1976287784b0645ebde6445
Wayne Olson <weepler@D-69-91-128-184.dhcp4.washington.edu>
2018-04-19 17:29:19 -0700

    ghis is a strint done

--- students/Wayne/Assignments/Lesson3/SlicingLabExercise.py
+++ students/Wayne/Assignments/Lesson3/SlicingLabExercise.py
@@ -1,6 +1,21 @@
 # Slicing Lab Exercise
 
+
+# a_string = "this is a string"
+# a_tuple = (2, 54, 13, 12, 5, 32)
+
+# assert exchange_first_last(a_string) == "ghis is a strint"
+# assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
+
 # with the first and last items exchanged.
+
+a = 'This is a string'
+
+# returns 'ghis is a string'
+
+a[-1] + a[1:-1] + a[0]
+
+
 
 
 # with every other item removed.
