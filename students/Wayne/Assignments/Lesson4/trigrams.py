91f160f0d9ddf3f6ddd376ebcbed5ce5060d2f0a
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-04-19 21:43:13 -0700

    adding work done on trigrams assginment during class

--- students/Wayne/Assignments/Lesson4/trigrams.py
+++ students/Wayne/Assignments/Lesson4/trigrams.py
@@ -11,22 +11,25 @@ His rooms were to know brilliantly lit"""
 
 words = sample.split()
 
+import random
+
 print(words)
 
 def parse_file(filename):
     """
     parse text file to makelist of words
     """
+    words = []
     with open(filename) as infile:
-        words = [ 
+        for _ in range(61):
+            infile.readline()
         for line in infile:
-            print(line)
+            if line.startswith() == "End of the Project Gutenberg":
+                break
             wrds = line.split().split()
             print(wrds)
             words.extend(wrds)
-            
         return words
-
 
 
 def build_trigrams(words):
@@ -43,6 +46,19 @@ def build_trigrams(words):
     return tris
 
 
+def make_new_text(trigram):
+    pair = random.choice(list(trigram.keys()))
+    sentence = []
+    sentence.extend(pair)
+    for i in range(10):
+        followers = trigram[pair]
+        sentence.append(random.choice(followers))
+        pair = tuple(sentence [-2:])
+        print(pair)
+    return sentence
+
+
 if __name__ == "__main__":
-    words = parse_file("")
-    #trigram = build_trigrams(words)
+    words = parse_file("/Users/weepler/Python210/Sp2018-Accelerated/students/Wayne/Assignments/Lesson4/sherlock.txt")
+    trigram = build_trigrams(words)
+    make_new_text(trigram)
