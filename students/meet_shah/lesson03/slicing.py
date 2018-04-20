167b15e825c5cb8a5c6277649d61e7e7db8d8578
Shah <meet29in@gmail.com>
2018-04-19 17:53:29 -0700

    Need to return as a slice not a single element

--- students/meet_shah/lesson03/slicing.py
+++ students/meet_shah/lesson03/slicing.py
@@ -2,15 +2,9 @@ a_string = "this is a string"
 a_tuple = (2, 54, 13, 12, 5, 32)
 a_list = [3,4,5,6,7,8]
 def exchange_first_last(seq):
-   first = seq[0]
-   middle = seq[1:len(seq)-1]
-   last = seq[-1]
-   newseq = middle.append(first)
-   print(newseq)
-   return newseq
+   return seq[-1:] + seq[1:-1] + seq[:1]
 
-#print(exchange_first_last(a_tuple))
-print(exchange_first_last(a_list))
-#assert exchange_first_last(a_string) == "ghis is a strint"
+assert exchange_first_last(a_list) == [8,4,5,6,7,3]
+assert exchange_first_last(a_string) == "ghis is a strint"
 assert exchange_first_last(a_tuple) == (32, 54, 13, 12, 5, 2)
 
