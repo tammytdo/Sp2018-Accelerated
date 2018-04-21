c05b1f3b1589b090dd3ecb5142ff939d23ece7b6
Ryan Drovdahl <rdrovdahl@Ryans-MacBook-Pro.local>
2018-04-21 16:02:01 -0700

    cleaned up mailroom.py and added comments

--- students/drovdahl/lesson03/mailroom.py
+++ students/drovdahl/lesson03/mailroom.py
@@ -8,7 +8,7 @@ simple mail program for non profit
 import sys
 import os
 
-donors = [('Iron Man', [1000000, 5000000, 10000000]),
+donors = [('Iron Man', [100000, 50000, 1000]),
           ('Thor', [50, 25, 100]),
           ('Hulk', [500]),
           ('Captain America', [30, 40]),
@@ -20,12 +20,25 @@ donors = [('Iron Man', [1000000, 5000000, 10000000]),
 
 
 def thankyou():
+    '''
+    Function which will allow user to type 'list' to see a list of donors.
+    The user can also add a new donation to either an existing donor or a
+    brand new donor.
+    The global list 'donors' will be modified with any new donors and/or
+    donations.
+    Once a new donation has been made, a THANK YOU email will be printed to the
+    screen.
+    '''
     global donors
-    print('So, you just recieved a new donation.  That\'s great!')
-    print('Let\'s record the donation and draft a THANK YOU message\n')
-    print('Who is the donor? Type the name of the donor or "list" to see a'
-          'list of existing donors.\n(type "quit" at any time to return to the'
-          'main menu)')
+    print('''So, you just recieved a new donation.  That's great!
+
+Let's record the donation and draft a THANK YOU message.
+
+
+Who is the donor? Type the name of the donor or type 'list' to see a
+list of existing donors.
+(type 'quit' at any time to return to the main menu)''')
+# collect the donor name
     donor = input('>> ')
     if donor.lower() == 'quit':
         return
@@ -38,10 +51,16 @@ def thankyou():
         donor = input('>> ')
         if donor.lower() == 'quit':
             return
+# collect the donation amount
     donor_donation = input('What is the donation amount? \n >> ')
+# normallize the donation amount
     donor_donation = donor_donation.replace('$', '').replace(',', '')
     donor_donation = float(donor_donation)
-#    print('%.2f' % donor_donation)
+# iterate through the donors list to see if the donor already exists
+# if the donor already exists, append the new donation amount to the existing
+# list
+# if the donor is not matched, add a new donor/donation amount entry to the
+# donors list
     x = 0
     name_match = False
     for name in donors:
@@ -68,25 +87,42 @@ def thankyou():
 
 
 def report():
+    '''
+    This report will print a list of donors, total donated, number of
+    donations,, and average donation amounts as values in each row.
+    Using string formatting, the output rows will line up as nice as possible.
+    The end result will be tabular (values in each column should align with
+    those above and below)
+    '''
     os.system('clear')
+    # print top row
     print('Donor Name                | Total Given | Num Gifts | Average Gift',
           '\n' + ('-' * 66))
+    # length of first cell
     a1 = ' ' * 28
+    # length of second and forth cell
     a2 = ' ' * 11
-    a3 = ' ' * 7
-    a4 = ' ' * 7
-    a5 = ' ' * 11
+    # length of third cell(first half)
+    a3_1 = ' ' * 7
+    # length of third cell(second half)
+    a3_2 = ' ' * 7
+    # length of forth cell
+    # a5 = ' ' * 11
     for x in donors:
         donor = x[0]
         gift_total = round(sum(x[1]), 2)
         gift_number = len(x[1])
         gift_average = round((sum(x[1])/len(x[1])), 2)
-        b1 = donor + a1[len(donor):]
-        y = 11 - len(str(gift_total))
-        z = 7 - len(str(gift_number))
-        zz = 11 - len(str(gift_average))
-        print(b1 + '$' + a2[0:y] + str(gift_total) + a3[0:z] + str(gift_number)
-              + a4 + '$' + a5[0:zz] + str(gift_average))
+        # assign the output of the first cell
+        c1 = donor + a1[len(donor):]
+        # assign the value of the second cell
+        c2 = '$' + a2[0:-(len(str(gift_total)))] + str(gift_total)
+        # assign the value of the third cell
+        c3 = a3_1[0:-(len(str(gift_number)))] + str(gift_number) + a3_2
+        # assign the value of the forth cell
+        c4 = '$' + a2[0:-(len(str(gift_average)))] + str(gift_average)
+        # print the row
+        print(c1 + c2 + c3 + c4)
     print('\n\n')
 
 
@@ -95,6 +131,9 @@ def quit():
 
 
 def mainloop():
+    '''
+    Main menu function
+    '''
     os.system('clear')
     print('''Avengers: Fund-a-Kitten Initiative
   Because every little girl
