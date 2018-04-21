fdf891597f29904ddad7fb8ae95ae02cd673d5b3
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-04-20 21:06:00 -0700

    trying to started building thankyou function and figuring out how to print donors;

--- students/Wayne/Assignments/Lesson3/Mailroom.py
+++ students/Wayne/Assignments/Lesson3/Mailroom.py
@@ -12,11 +12,33 @@ donors = [("Donor A",[50,100,24,100]),
         ]
 
 
+#def thankyou():
+    #print("Please Input the Donor you would like to thank\n")
+
+def list_donors():
+    print("Donors:\n")
+    for donor in donors:
+        print(donor[0])
+
+
+
+
 def thankyou():
-    print("This is the thank you\n")
+    while True:
+        name = input ("Enter the donor's name or a list of donor's"
+                      "(or a 'list' to see all donors or type 'menu' to exit)>").strip()
+        if name == "list":
+            print(name)
+        elif name == "menu":
+            return
+        else:
+            break
+
+
+
 
 def report():
-    print ("This is the report function\n")
+    print("This is the report function\n")
 
 def quit():
     sys.exit()
@@ -31,21 +53,21 @@ def mainloop():
               "3)Quit\n")
         response = input('>>')
 
-        print("response was:", response)
-        if response not in ("1","2","3"):
-            print("Not a Valid Response, input 1, 2, or 3***\n")
-            continue
-        elif response == "1":
-            thank_you()
-        elif response == "2":
-            report()
-        elif response == "3":
-            quit()
-            
+ 
 
 
 
 if __name__ == "__main__":
-    mainloop()
-
-
+    running = True
+    while running:
+        response = mainloop()
+        inputdonorname = input()
+        if response not in ("1", "2", "3"):
+            print("Not a Valid Response, input 1, 2, or 3***\n")
+            continue
+        elif response == "1":
+            thankyou()
+        elif response == "2":
+            report()
+        elif response == "3":
+            quit()
