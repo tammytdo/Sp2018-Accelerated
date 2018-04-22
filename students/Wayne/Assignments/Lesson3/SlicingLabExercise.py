43439a981ac795c43f26f800cfcf53609dd6d7c1
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-04-21 22:00:57 -0700

    completed reverse string and everyother string

--- students/Wayne/Assignments/Lesson3/SlicingLabExercise.py
+++ students/Wayne/Assignments/Lesson3/SlicingLabExercise.py
@@ -1,7 +1,7 @@
 # Slicing Lab Exercise
 
 
-# a_string = "this is a string"
+a_string = "this is a string"
 # a_tuple = (2, 54, 13, 12, 5, 32)
 
 # assert exchange_first_last(a_string) == "ghis is a strint"
@@ -9,27 +9,33 @@
 
 # with the first and last items exchanged.
 
-def exchange_first_last(a_string):
-    a_string = input()
-    a_tuple = 'taco'
-    return (a_string[-1:] + a_string[1:-1] + a_string[:1])
-    a_string[-1] + a_string[1:-1] + a_string[0]
-    tuple[-1:] + a_tuple[1:-1] + a_tuple[:1]
+# # def exchange_first_last(a_string):
+#     a_string = input()
+#     a_tuple = 'taco'
+#     return (a_string[-1:] + a_string[1:-1] + a_string[:1])
+#     a_string[-1] + a_string[1:-1] + a_string[0]
+#     tuple[-1:] + a_tuple[1:-1] + a_tuple[:1]
 
-assert exchange_first_last ('This is a string') == "ghis is a strint"
+# assert exchange_first_last ('This is a string') == "ghis is a strint"
 
-print('exchange_first_last passed assert test')
+# print('exchange_first_last passed assert test')
 
 
 
 # with every other item removed.
 
+everyotherstr = a_string[1::2]
+
+print(everyotherstr)
 
 # with the first and last 4 items removed, and every other item in between.
 
 
 # with the elements reversed (just with slicing).
 
+reversestr = a_string[::-1]
+
+print(reversestr)
 
 # with the middle third, then last third, then the first third
 # in the new order.
