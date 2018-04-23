c7f0dd46e34f10c508a336eddcc6d42a5d9689e3
Andy <kwona714@hotmail.com>
2018-04-23 01:17:51 -0700

    Andy K - commit trigrams.py

--- students/andykwon/lesson04/trigrams.py
+++ students/andykwon/lesson04/trigrams.py
@@ -3,70 +3,67 @@
 """
 trigrams:
 
-solution to the trygrams project
+solution to the trigrams project
 """
+
 import random
+import re
 
 sample = """I was seized with a keen desire to see Holmes
-again and to know how he was employing his extraordinary powers
-His rooms were brilliantly lit"""
+again with a and to know how he was to see employing his extraordinary powers
+His rooms were to know brilliantly lit"""
 
 words = sample.split()
+
+# print(words)
 
 def parse_file(filename):
     """
     parse text file to make list of words
     """
-
-    words = []
+    words =[]
     with open(filename) as infile:
         for _ in range(61):
             infile.readline()
         for line in infile:
             if line.startswith("End of the Project Gutenberg"):
                 break
-            wrds = line.strip().split()
+            # includes punctuation as a string
+            wrds = re.findall("[\w']+|[.,!?;]", line)
             words.extend(wrds)
-
     return words
-
 
 def build_trigram(words):
     tris = {}
     for i in range(len(words) - 2):
-        first = words[i]
-        second = words[i + 1]
-        third = words[i + 3]
-        tris[(first, second)] = [third]
+        # first = words[i]
+        # second = words[i + 1]
+        # third = words[i + 2]
+        # pair = (first, second)
+        pair = tuple(words[i:i+2])
+        third = words[i+2]
 
-        pair = (first, second)
-
-        tris.setdefault(pair, []).append(third)
-        # list_of_followers = tris.setdefault(pair.[])
-        # list_of_followers.append(third)
-
-        # if pair in tris:
-        #     tris[pair].append(third)
-        # else:
-        #     tris[pair] = [third]
+        list_of_followers = tris.setdefault(pair, [])
+        list_of_followers.append(third)
 
     return tris
+
 
 def make_new_text(trigram):
     pair = random.choice(list(trigram.keys()))
     sentence = []
     sentence.extend(pair)
-    
-    for i in range (10):
+    for i in range(20):
         followers = trigram[pair]
         sentence.append(random.choice(followers))
         pair = tuple(sentence[-2:])
-    return setence
-
-
-
+    return sentence
 
 if __name__ == "__main__":
-    words = parse_file("sherlock_small.txt")
+    words = parse_file("sherlock.txt")
+    # print(words)
     trigram = build_trigram(words)
-    make_new_text(trigram)
+    text = make_new_text(trigram)
+    print(text)
+
+
