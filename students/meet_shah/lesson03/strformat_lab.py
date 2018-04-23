6b67e120f7052dc67da8f96291f3151ec079cf66
Shah <meet29in@gmail.com>
2018-04-23 10:16:22 -0700

    Meet Shah: Lesson03: String format added task6 in strformat_lab.py

--- students/meet_shah/lesson03/strformat_lab.py
+++ students/meet_shah/lesson03/strformat_lab.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 
 def task1(t):
     return "file_{:03}: {:.02f}, {:.2e}, {:.3e}".format(*t)
@@ -7,8 +7,6 @@ assert task1((2, 123.4567, 10000, 12345.67)) == 'file_002: 123.46, 1.00e+04, 1.2
 
 def task2(t):
     return t
-
-#assert task2((2, 123.4567, 10000, 12345.67)) == 'file_002: 123.46, 1.00e+04, 1.235e+04'
 
 def task3(t):
     return ("the {} numbers are: "+ ", ".join(["{:d}"]*len(t))).format(len(t), *t)
@@ -25,3 +23,12 @@ def task5(t):
     return f'The weight of an {t[0]} is {t[1]} and the weight of a {t[2]} is {t[3]}'
 
 assert task5(['oranges', 1.3, 'lemons', 1.1]) == "The weight of an oranges is 1.3 and the weight of a lemons is 1.1"
+
+
+def task6(t):
+    return '{:20}{:>6}{:>25}'.format(*t)
+
+print(task6(('Name', 'Age', 'Cost')))
+print(task6(('Hello', '10', '200')))
+print(task6(('Yello', '50', '4000')))
+print(task6(('Bello', '100', '50000')))
