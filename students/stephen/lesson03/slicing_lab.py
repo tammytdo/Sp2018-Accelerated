4bf3b161dfb8f470155564d4e8e8ee945a7d5548
Stephen <ssoukasene@gmail.com>
2018-04-22 19:56:47 -0700

    Adding slicing lab and mailroom

---
+++ students/stephen/lesson03/slicing_lab.py
@@ -0,0 +1,11 @@
+# Write some functions that take a sequence as an argument,
+# and return a copy of that sequence:
+# with the first and last items exchanged.
+# with every other item removed.
+# with the first and last 4 items removed, and every other item in between.
+# with the elements reversed (just with slicing).
+# with the middle third, then last third, then the first third in the new order.
+
+
+def first_last(seq):
+    return seq[:]
