ee59ab5e68db5f8791ae3016f24ba15acccbfa62
Shah <meet29in@gmail.com>
2018-04-23 00:57:14 -0700

    Lesson 3: string lab tasks 1, 3, 4, 5 complete. Instructions seem unclear on what task 2 needs.

---
+++ students/meet_shah/lesson03/strformat_lab.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def task1(t):
+    return "file_{:03}: {:.02f}, {:.2e}, {:.3e}".format(*t)
+
+assert task1((2, 123.4567, 10000, 12345.67)) == 'file_002: 123.46, 1.00e+04, 1.235e+04'
+
+def task2(t):
+    return t
+
+#assert task2((2, 123.4567, 10000, 12345.67)) == 'file_002: 123.46, 1.00e+04, 1.235e+04'
+
+def task3(t):
+    return ("the {} numbers are: "+ ", ".join(["{:d}"]*len(t))).format(len(t), *t)
+
+assert(task3((1,2,3))) == "the 3 numbers are: 1, 2, 3"
+assert(task3((3,4,5,6,7))) == "the 5 numbers are: 3, 4, 5, 6, 7"
+
+def task4(t):
+    return ("{:02} {:02} {:02} {:02} {:02}").format(t[3],t[4],t[2],t[0],t[1])
+
+assert task4((4, 30, 2017, 2, 27)) == "02 27 2017 04 30"
+
+def task5(t):
+    return f'The weight of an {t[0]} is {t[1]} and the weight of a {t[2]} is {t[3]}'
+
+assert task5(['oranges', 1.3, 'lemons', 1.1]) == "The weight of an oranges is 1.3 and the weight of a lemons is 1.1"
