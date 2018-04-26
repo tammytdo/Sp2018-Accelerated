dcf0fb582e9ae26348e80408805d5b28c0d59a9f
Siddiquee <n0291178@lm.lmig.com>
2018-04-25 23:32:03 -0700

    unclean proof of concept of trigrams - need to delve further into punctuation sophistication

--- students/Osiddiquee/lesson04/trigram.py
+++ students/Osiddiquee/lesson04/trigram.py
@@ -23,20 +23,12 @@ def random_trigram(list):
     return random.choice(list)
 
 
-def list_add(trigram, list):
-    kata_list = []
-    return kata_list.append(random_trigram(trigram[list[-2] + ' ' + list[-1]]))
-
 def text_generator(text, start, num_words = 10):
     trigrams = dictionary_builder(text)
     if start not in trigrams:
         print('The phrase you provided doesn\'t exist in our training set')
     else:
-        kata_list = start.split(' ')
-        try:
-            while len(kata_list) < num_words:
-                addition = list_add(trigrams, kata_list)
-                kata_list = addition
-            return ' '.join(kata_list)
-        except TypeError:
-                print(kata_list)
+        word_list = start.split(' ')
+        while len(word_list) < num_words:
+            word_list.append(random_trigram(trigrams[word_list[-2] + ' ' + word_list[-1]]))
+        return ' '.join(word_list)
