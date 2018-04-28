7baa366f6cd071df963f57cf33e8ff37ec48458a
mathewcmartin <chactasstryker@gmail.com>
2018-04-28 11:36:00 -0700

    Adding reports to mailroom.py

--- students/mathewcm/lesson03/mailroom.py
+++ students/mathewcm/lesson03/mailroom.py
@@ -25,13 +25,13 @@ import sys
 
 donors = ["John Smith", "Ringo Eclipse", "Burt Syran", "Lance J. Johnson", 
           "Orel Winfrey"]
-dues = [[25, 25], [25, 25, 25, 25], [50, 50, 50, 150], [25, 25, 50, 25], [500]]
+donations = [[25, 25], [25, 25, 25, 25], [50, 50, 50, 150], [25, 25, 50, 25], [500]]
 
 # Global variable for donors and dues paid
 
-DONORS = dict(zip(donors, dues))
+DONORS = dict(zip(donors, donations))
 del donors
-del dues
+del donations
 
 def thank_you():
     print("This is a thank_you function")
@@ -46,6 +46,59 @@ def quit():
 def return_to_menu():
     """ returns true to exit out of subloop: from solutions"""
     return True
+
+
+def create_donors_report():
+    [name, total, number, avg] = [[], [], [], []]
+    for x in DONORS:
+        name.append(x)
+        total.append(sum(DONORS[x]))
+        number.append(len(DONORS[x]))
+        avg.append(total[-1] / number[-1])
+    donors_report = list(zip(name, total, number, avg))
+    donors_report = sorted(
+            donors_report, key=lambda y: int(y[1]), reverse=True)
+    return donors_report
+
+def config_donors_report():
+    print_report(create_donors_report())
+
+def report_display_config():
+    """Configures display of donors_report in terminal"""
+    headers = ('Donor Name', 'Total donation', '# of donations', 'Avg donation')
+    data = '|'.join(["{:<40}", "{:<12}", "{:<10}", "{:<12}"])
+    width = len(data.format(*headers))
+    head = (data.format(*headers)) + '\n'
+    head = head + (width * "-") + '\n'
+    return head
+
+def report_body_config(list_data):
+    """Configures display of donors_report body"""
+    body = ""
+    data = ''.join(["{:<40}", "${:^12.2f}", "{:^12d}", "${:^12.2f}"])
+    for i in range(len(list_data)):
+        entry = list_data[i]
+        body = body + data.format(*entry) + '\n'
+    return body
+
+def print_report(list_data):
+    print(report_display_config())
+    print(report_body_config(list_data))
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 def mainloop():
