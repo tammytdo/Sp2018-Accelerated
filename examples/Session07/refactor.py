2999e16071c5d81ae648b4c3e7ed60496621c466
Chris Barker <PythonCHB@gmail.com>
2018-04-30 21:04:14 -0700

    added start of html render exercise

--- examples/Session07/refactor.py
+++ examples/Session07/refactor.py
@@ -9,15 +9,14 @@ def test_data():
     return dict(zip(donors, donations))
 
 
+def pack(don):
+    n, don = don
+    total = sum(don)
+    return (n, total, len(don), (total / len(don)))
+
+
 def create_donors_report(DONORS):
-    [name, total, number, avg] = [[], [], [], []]
-    for x in DONORS:
-        name.append(x)
-        total.append(sum(DONORS[x]))
-        number.append(len(DONORS[x]))
-        avg.append(total[-1] / number[-1])
-    donors_report = list(zip(name, total, number, avg))
-    donors_report = sorted(donors_report,
-                           key=lambda y: int(y[1]), reverse=True)
-    return donors_report
+    donor_report = sorted([pack(don) for don in DONORS.items()],
+                          key=lambda y: y[1], reverse=True)
+    return donor_report
 
