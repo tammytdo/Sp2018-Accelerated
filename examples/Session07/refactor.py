bcb841794dc0a0d1ce2ceeabecc540dcd181a62c
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-05-01 19:54:01 -0700

    adding changes to test mailroom and the html render changes

--- examples/Session07/refactor.py
+++ examples/Session07/refactor.py
@@ -8,6 +8,19 @@ def test_data():
     donations = [[25, 25], [25, 25, 25, 25], [50, 50, 50, 150], [25, 25, 50, 25], [500]]
     return dict(zip(donors, donations))
 
+# creates a list and unpacks it
+# looping through the donors and computing name, total, number etc
+# donor is dictionary and x is looking at the name key
+# changed variable to n for styling
+# zip returns an iterable of tuples
+# removed the list
+# lambda allows you to write little key functons into your cold
+# a function that is passed an item will return what you want to
+# sort by
+# key function returns index 1
+
+
+
 
 def create_donors_report(DONORS):
     [name, total, number, avg] = [[], [], [], []]
@@ -21,3 +34,24 @@ def create_donors_report(DONORS):
                            key=lambda y: int(y[1]), reverse=True)
     return donors_report
 
+#after
+
+def create_donors_report(DONORS):
+    [name, total, number, avg] = [], [], [], []
+    donors_report = []
+    for n, don in DONORS.items():
+        donor_report.append(n,
+                          sum(don),
+                          len(don)
+                          (total[-1] / number[-1])
+                          )
+        name.append(n)
+        total.append(*sum(don))
+        number.append(len(don))
+        avg.append(total[-1] / number[-1])
+
+    def key(y):
+        return y[1]
+    donor_report = sorted(zip(name, total, number, avg),
+                           key=key, reverse=True)
+    return donors_report
---
+++ students/Wayne/Assignments/Lesson7/html_render.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+"""
+A class-based system for rendering html.
+"""
+
+
+# This is the framework for the base class
+class Element:
+    tag = "html"
+
+    def __init__(self, content=None):
+
+        if content is not None:
+            self.content = []
+        else:
+            self.content = [content]
+
+    def append(self, new_content):
+        self.content.append(new_content)
+
+
+    def open_Tag(self):
+        return f"<{self.tag}>
+
+    def render(self, out_file):
+        out_file.write(f"<{self.tag}>\n")
+        for line in self.content:
+            if isinstance(line, str):
+                out_file.write(line)  # passes in a the class to render
+                out_file.write("\n")
+            else:
+                line.render(out_file)
+        out_file.write("</{}>\n".format(self.tag))
+
+    def onelinerender(self, out_file):
+        out_file.write(f"<{self.tag}>")
+        for line in self.content:
+            if isinstance(line, str):
+                out_file.write(line)  # passes in a the class to render
+            else:
+                line.render(out_file)
+        out_file.write("</{}>\n".format(self.tag))
+
+
+"""
+Class P inherits the functionality from Element
+"""
+
+
+class P(Element):
+    tag = "p"
+
+
+class Html(Element):
+    tag = "html"
+
+
+class Body(Element):
+    tag = "body"
+
+
+class Head(Element):
+    tag = "Head"
+
+class Title(OneLineTag):
+    tag = "Title"
