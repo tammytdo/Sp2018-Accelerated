346d7a3ed1e09972a5278c13219f25c3f6ac659f
dwightjay <johnsd2@uw.edu>
2018-05-03 17:01:06 -0700

    lesson07 on step 5

--- examples/Session07/refactor.py
+++ examples/Session07/refactor.py
@@ -10,14 +10,17 @@ def test_data():
 
 
 def create_donors_report(DONORS):
-    [name, total, number, avg] = [[], [], [], []]
-    for x in DONORS:
-        name.append(x)
-        total.append(sum(DONORS[x]))
-        number.append(len(DONORS[x]))
+    name, total, number, avg = [], [], [], []
+    #donor_info = []
+    for n, don in DONORS.items():
+        name.append(n)
+        total.append(sum(don))
+        number.append(len(don))
         avg.append(total[-1] / number[-1])
     donors_report = list(zip(name, total, number, avg))
     donors_report = sorted(donors_report,
                            key=lambda y: int(y[1]), reverse=True)
     return donors_report
 
+
+def pack()
