137ee0308fff681d4cacb5d0eaac8b69474be47c
Stephen <ssoukasene@gmail.com>
2018-05-05 23:48:05 -0700

    Completed circle.py exercise

--- students/stephen/lesson08/circle.py
+++ students/stephen/lesson08/circle.py
@@ -4,22 +4,81 @@ class Circle:
     """
     Class that represents the properties of a circle
     """
-    def __init__(self, the_radius):
-        self._radius = the_radius
-        self._diameter = 2 * the_radius
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("The radius of the circle can't be less than or equal to 0")
+        else:
+            self._radius = radius
+
+    def __repr__(self):
+        return 'Circle({})'.format(self.radius)
+
+    def __str__(self):
+        return 'Circle with radius: {}'.format(self.radius)  
     
     @property
     def radius(self):
         return self._radius
     @radius.setter
-    def radius(self, value):
-        self._radius = value
-        self._diameter = 2 * value
+    def radius(self, val):
+        if val <= 0:
+            raise ValueError("The radius of the circle can't be less than or equal to 0")
+        else:
+            self._radius = val
 
     @property
     def diameter(self):
-        return self._diameter
+        return self.radius * 2
     @diameter.setter
-    def diameter(self, value):
-        self._diameter = value
-        self._radius = value / 2
+    def diameter(self, val):
+        self.radius = val / 2
+
+
+    @property
+    def area(self):
+        """
+        An area property so the user can get the area of the circle
+        """
+        return math.pi * (self.radius ** 2)
+
+    @classmethod
+    def from_diameter(cls, diameter):
+        """
+        An alternate constructor that lets the user create a Circle directly with the diameter
+        """
+        return cls(diameter / 2)
+
+    # equality protocol for tests
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    # Numeric Protocols
+    # Add Circles
+    # c1 = Circle(2)
+    # c2 = Circle(4)
+    # c1 + c2
+    # Out[9]: Circle(6)
+    # and multiply one times a number:
+    # In [16]: c2 * 3
+    # Out[16]: Circle(12)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    def __rmul__(self, other):
+        return Circle(self.radius * other)
+
+    # Circle comparisons
+    def __ne__(self, other):
+        return self.radius != other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+
--- students/stephen/lesson08/test_circle.py
+++ students/stephen/lesson08/test_circle.py
@@ -1,10 +1,81 @@
 import pytest
+import math
 from circle import Circle
+
+
+def test_init():
+    Circle(5)
+    with pytest.raises(TypeError):
+        Circle()
+    with pytest.raises(ValueError):
+        Circle(-5)
+    with pytest.raises(ValueError):
+        Circle(0)
+
 
 def test_radius():
     c = Circle(4)
     assert c.radius == 4
+    c.radius = 5
+    assert c.radius == 5
+    with pytest.raises(ValueError):
+        c.radius = -5
+    with pytest.raises(ValueError):
+        c.radius = 0
+    
 
 def test_diameter():
     c = Circle(5)
     assert c.diameter == 10
+    c.diameter = 20
+    assert c.diameter == 20
+    assert c.radius == 10
+    with pytest.raises(ValueError):
+        c.diameter = -10
+    with pytest.raises(ValueError):
+        c.diameter = 0
+
+def test_area():
+    c = Circle(5)
+    assert c.area == (5 ** 2) * math.pi
+    with pytest.raises(AttributeError):
+        c.area = 36
+
+def test_from_diameter():
+    c = Circle.from_diameter(50)
+    assert c.radius == 25
+    assert c.diameter == 50
+    assert c.area == (25 ** 2) * math.pi
+
+def test_repr():
+    c = Circle(9)
+    assert repr(c) == 'Circle(9)'
+
+def test_str():
+    c = Circle(13)
+    assert str(c) == 'Circle with radius: 13'
+
+def test_add():
+    c1 = Circle(5)
+    c2 = Circle(2)
+    assert (c1 + c2) == Circle(7)
+    assert (c1 + c2) == (c2 + c1)
+
+def test_mul():
+    c1 = Circle(5)
+    assert (c1 * 3) == Circle(15)
+    assert (3 * c1) == Circle(15)
+    assert (3 * c1) == (c1 * 3)
+
+def test_comparisons():
+    assert Circle(2) == Circle(4 / 2)
+    assert Circle(2) > Circle(1)
+    assert Circle(2) < Circle(3)
+    assert Circle(2) <= Circle(2)
+    assert Circle(2) <= Circle(3)
+    assert Circle(2) >= Circle(2)
+    assert Circle(2) >= Circle(1)
+
+def test_sort():
+    circles = [Circle(2), Circle(9), Circle(4), Circle(1)]
+    assert sorted(circles) == [Circle(1), Circle(2), Circle(4), Circle(9)]
