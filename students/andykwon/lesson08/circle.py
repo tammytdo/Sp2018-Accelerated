256f489660711d538951f424b82cc45abeed9457
Andy <kwona714@hotmail.com>
2018-05-06 10:20:50 -0700

    Andy K - Commmit circle class code and test.

---
+++ students/andykwon/lesson08/circle.py
@@ -0,0 +1,71 @@
+import math
+
+class Circle():
+
+    def __init__(self, r = None):
+
+        if r is None:
+            self.radius = 0
+        else:
+            self.radius = r
+
+    def __str__(self):
+        return 'Circle with radius: %d' % (self.radius)
+
+    def __repr__(self):
+        return 'Circle(%d)' % (self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    # def __rmul__(self, other):
+    #     return Circle(self.radius * other)
+    __rmul__ = __mul__
+
+    def __gt__(self, other):
+        if self.radius > other.radius:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.radius < other.radius:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):
+        if self.radius == other.radius:
+            return True
+        else:
+            return False
+
+    @property
+    def diameter(self):
+        return 2 * self.radius
+    @diameter.setter
+    def diameter(self, val):
+        self.radius = val / 2
+
+    @property
+    def area(self):
+        return math.pi * self.radius**2
+
+    @classmethod
+    def from_diameter(class_object, d = None):
+        return class_object(d / 2)
+
+    # def radius(self):
+
+    #     return self.r
+
+    # def diameter(self):
+
+    #     return self.r * 2
+
+    # def area(self):
+
+    #     return math.pi * self.r**2
---
+++ students/andykwon/lesson08/test_circle.py
@@ -0,0 +1,152 @@
+#!/usr/bin3/env python3
+
+from circle import Circle
+
+import pytest
+import unittest
+
+
+# Step 1:
+def test_intialize():
+    """
+    Test that the radius gets initialized
+    """
+    c = Circle()
+    c = Circle(4)
+
+
+def test_radius():
+    """
+    Test that the radius is the radius
+    """
+    c = Circle(4)
+
+    assert c.radius == 4
+
+
+# Step 2:
+def test_diameter():
+    """
+    Test that the diameter gets calculated properly
+    """
+    c = Circle(4)
+
+    assert c.diameter == 8
+
+
+# Step 3:
+def test_diameter_param():
+    """
+    Test that the diameter property can be inputted, changing
+    both the diameter and radius properties appropriately
+    """
+
+    c = Circle(4)
+    c.diameter = 2
+
+    assert c.diameter == 2
+    assert c.radius == 1
+
+
+# Step 4:
+def test_area():
+    """
+    Test that the area gets calculated properly
+    """
+    c = Circle(4)
+
+    assert c.area == 50.26548245743669
+
+# Not sure how to test for errors...
+# def test_area_param():
+
+#     c = Circle(4)
+
+#     with assertRaises(AttributeError):
+#         c.area = 42
+
+
+# Step 5: 
+def test_alt_constructor():
+    """
+    Test that the alternate constructor of from_diameter() works.
+    If the alternate constructor is used to create a Circle,
+    radius should be updated accordingly.
+    """
+    c = Circle.from_diameter(4)
+
+    assert c.diameter == 4
+    assert c.radius == 2
+
+
+# Step 6: 
+def test_print():
+    """
+    Test that printing a Circle class object prints the string
+    that is in __str__ 
+    """
+    c = Circle(4)
+    # Not sure yet on how to test a print...
+    # assert print(c) == "Circle with radius: 4"
+
+
+# Step 7:
+def test_add():
+    """
+    Test that adding two Circle classes together yields a
+    Circle class with sums of their radii
+    """
+    c1 = Circle(2)
+    c2 = Circle(4)
+
+    assert c1 + c2 == Circle(6)
+
+
+# Step 8:
+def test_greater_than():
+    """
+    Test that the Circles can be compared with greater than statement
+    """
+    c1 = Circle(2)
+    c2 = Circle(4)
+
+    result1 = c1 > c2
+    result2 = c2 > c1
+
+    assert result1 is False
+    assert result2 is True
+
+
+def test_less_than():
+    """
+    Test that the Circles can be compared with less than statement
+    """
+
+    c1 = Circle(2)
+    c2 = Circle(4)
+
+    result1 = c1 < c2
+    result2 = c2 < c1
+
+    assert result1 is True
+    assert result2 is False
+
+
+def test_equal_to():
+    """
+    Test that the Circles can be compared to see if they're equal
+    """
+    c1 = Circle(4)
+    c2 = Circle(4)
+    c3 = Circle(2)
+
+    result1 = c1 == c2
+    result2 = c1 == c3
+
+    assert result1 is True
+    assert result2 is False
+
+
+
+
+
