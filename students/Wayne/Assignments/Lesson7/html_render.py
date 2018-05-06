8b15896d79cb5a201a2b5cc086e8e61aeaf71ccb
Wayne Olson <weepler@D-10-18-221-173.dhcp4.washington.edu>
2018-05-06 15:59:53 -0700

    Commit to show html_render.ph

--- students/Wayne/Assignments/Lesson7/html_render.py
+++ students/Wayne/Assignments/Lesson7/html_render.py
@@ -7,11 +7,16 @@ A class-based system for rendering html.
 
 # This is the framework for the base class
 class Element:
+
     tag = "html"
 
-    def __init__(self, content=None):
+    def __init__(self, content, **kwargs):
+        lst = []
+        for k in kwargs:
+            lst.append('{}= "{}"'.format(k, kwargs[k]))
+        self.atr = ' '.join(lst)
 
-        if content is not None:
+        if content is None:
             self.content = []
         else:
             self.content = [content]
@@ -19,24 +24,15 @@ class Element:
     def append(self, new_content):
         self.content.append(new_content)
 
-    def open_Tag(self):
-        return f"<{self.tag}>"
+    def open_tag(self):
+        return f"<{self.tag} {self.atr}>"
 
     def render(self, out_file):
-        out_file.write(f"<{self.tag}>\n")
+        out_file.write(self.open_tag() + "\n")
         for line in self.content:
             if isinstance(line, str):
                 out_file.write(line)  # passes in a the class to render
                 out_file.write("\n")
-            else:
-                line.render(out_file)
-        out_file.write("</{}>\n".format(self.tag))
-
-    def onelinerender(self, out_file):
-        out_file.write(f"<{self.tag}>")
-        for line in self.content:
-            if isinstance(line, str):
-                out_file.write(line)  # passes in a the class to render
             else:
                 line.render(out_file)
         out_file.write("</{}>\n".format(self.tag))
@@ -62,5 +58,18 @@ class Body(Element):
 class Head(Element):
     tag = "Head"
 
-#class Title(OneLineTag):
-#    tag = "Title"
+
+class OneLineTag(Element):
+
+    def render(self, out_file):
+        out_file.write(self.open_tag())
+        for line in self.content:
+            if isinstance(line, str):
+                out_file.write(line)  # passes in a the class to render
+            else:
+                line.render(out_file)
+        out_file.write("</{}>\n".format(self.tag))
+
+
+class Title(OneLineTag):
+    tag = "Title"
