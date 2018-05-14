01cb0b4f0d181b049fc1f748bb52347e4de75aab
Shah <meet29in@gmail.com>
2018-05-14 13:09:20 -0700

    Added filtering and projections

--- students/meet_shah/lesson10/donor_class.py
+++ students/meet_shah/lesson10/donor_class.py
@@ -56,6 +56,18 @@ class Donor:
         return new_donor
 
 
+    def filter_multiplier(self, factor, min_donation=0, max_donation=-1):
+        multiplier_donations = list(filter(lambda x: x > min_donation, self._donations))
+        if max_donation != -1:
+            multiplier_donations = list(filter(lambda x: x < max_donation, multiplier_donations))
+
+        non_multiplier_donations = list(filter(lambda x: x not in multiplier_donations, self._donations))
+        multiplier_donations = list(map(lambda x: x*factor, multiplier_donations))
+        new_donations = multiplier_donations + non_multiplier_donations
+        new_donor = Donor(self._name)
+        new_donor.add_donations(new_donations)
+        return new_donor
+
 class DonorDB:
 
     def __init__(self):
@@ -75,6 +87,10 @@ class DonorDB:
 
 
     def get_donor(self, donor_name):
+        return self._donors[donor_name.lower()]
+
+
+    def get_donor_information(self, donor_name):
         return self._donors[donor_name.lower()].information
 
     @property
@@ -106,3 +122,15 @@ class DonorDB:
          for _, donor in self._donors.items():
              new_donors.update_donor(donor.multiplier(multiplier))
          return new_donors
+
+    def donation_filter_multiple(self, multiplier, min_donation=0, max_donation=-1):
+        new_donors = DonorDB()
+        for _, donor in self._donors.items():
+            new_donors.update_donor(donor.filter_multiplier(multiplier, min_donation, max_donation))
+        return new_donors
+
+    def donor_projection(self, donor_nick, multiplier, min_donation=0, max_donation=-1):
+        # ((self.get_donor(donor_nick)).filter_multiplier(multiplier, min_donation, max_donation)).total_donations
+        current_donor = self.get_donor(donor_nick)
+        new_donor = current_donor.filter_multiplier(multiplier, min_donation, max_donation)
+        return new_donor.total_donations
