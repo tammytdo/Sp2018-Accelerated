daa358d37f42b0beaa115100b72fb582d92854b6
Shah <meet29in@gmail.com>
2018-05-14 17:46:50 -0700

    Added more tests

--- students/meet_shah/lesson06/mailroom_test.py
+++ students/meet_shah/lesson06/mailroom_test.py
@@ -17,3 +17,25 @@ testdonor={"Paul": [10000, 2000000, 5000, 550000]}
 # 					   Sincerely,
 # 					   The Team
 # """
+
+
+assert list_donors(testdonor) == "Paul"
+
+menu_string = """
+What do you want to do? Please choose from below options:
+1. Send a thank you
+2. Create a report
+3. Send letters to everyone
+4. Quit
+    """
+
+assert menu_generator("main") == menu_string
+
+thank_string = """
+Please choose from the below options
+1. List Donors
+2. Send a thank you
+3. Return to the main menu
+    """
+
+assert menu_generator("thank_you") == thank_string
