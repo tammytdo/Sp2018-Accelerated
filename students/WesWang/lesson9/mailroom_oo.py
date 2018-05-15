92cce523caaf6c47602709e71a037f3a86a7a2ed
Wesley Wang <wes82@uw.edu>
2018-05-14 19:32:06 -0700

    mailroom_oo modified

--- students/WesWang/lesson9/mailroom_oo.py
+++ students/WesWang/lesson9/mailroom_oo.py
@@ -6,8 +6,6 @@
 import sys
 from datetime import date
 
-donors = []
-
 class Donor:
     def __init__(self, name, donations = None):
         self._name = name
@@ -38,6 +36,10 @@ class Donor:
     def num_donation(self):
         return len(self._donations)
 
+    @property
+    def avg_donation(self):
+        return sum(self._donations)/len(self._donations)
+
     def new_donation(self, donation):
         self._donations.append(donation)
 
@@ -59,14 +61,44 @@ class Donor:
     def __ge__(self, other):
         return not sum(self._donations) >= sum(other._donations)
 
+class DonorDB():
+    def __init__(self):
+        self._donors = {}
+
+    @property
+    def donors(self):
+        return self._donors
+    
+    def add_donor(self, donor):
+        self._donors[donor.name] = donor
+
+    def generate_donor_list(self):
+        output = "-"*20 + "\nList of Donors:\n" + "-"*20
+        for donor in sorted(self._donors):
+            output += "\n" + donor
+        output += "\n" + "-"*20
+        return output
+
+    def build_report(self):
+        donors = self._donors
+        header = ["Donor Name","Total Given", "Num Gifts","Average Gifts"]
+        report = ""
+        report += "-"*75
+        report += f"\n{header[0]:30}| {header[1]:15}| {header[2]:10}| {header[3]:20}\n"
+        report += "-"*75
+        for donor in sorted(donors):
+            report += f"\n{donors[donor].name:30}${donors[donor].total:>15,.2f}{donors[donor].num_donation:>13}{donors[donor].avg_donation:>15,.2f}"
+        report += "\n" + "-"*75
+        return report
+
+
 def default_donors():
     donorA = Donor("Donor A", [10000,5000,3000])
     donorB = Donor("Donor B", [500,1200])
     donorC = Donor("Donor C", [100])
-    donors.extend([donorA, donorB, donorC])
-
-def add_donor(donor, amount):
-    donors.append(Donor(donor, [amount]))
+    db.add_donor(donorA)
+    db.add_donor(donorB)
+    db.add_donor(donorC)
 
 def thank_letter(donor, amount):
     letter = "-"*80 + f"""\nDear {donor},\n\n\tWe have received you donation of ${amount:,.2f}.
@@ -75,20 +107,11 @@ def thank_letter(donor, amount):
         \n\nSincerely,\n\nWesley Wang\n""" + "-"*80
     return letter
 
-
-def generate_donor_list():
-    output = "-"*20 + "\nList of Donors:\n" + "-"*20
-    for donor in sorted(donors, reverse = True):
-        output += "\n" + donor.name
-    output += "\n" + "-"*20
-    return output
-
-
 def thank_you():
     # Ask for user input
     response = input("Please enter donor's full name, or type 'list' to check all donors: ").title()
     if response.lower() == "list":
-        print(generate_donor_list())
+        print(db.generate_donor_list())
         thank_you()
 
     while True:
@@ -99,40 +122,30 @@ def thank_you():
             print("\nPlease enter a dollar amount!!\n")
             continue
 
+    donors = db.donors
     for donor in donors:
-        if response.lower() == donor.name.lower():
-            donor.new_donation(amount)
-            print(thank_letter(donor.name, amount))
+        if response.lower() == donors[donor].name.lower():
+            donors[donor].new_donation(amount)
+            print(thank_letter(donors[donor].name, amount))
             print("Return to main menu!!")
             mainloop()
-    add_donor(response, amount)
+    db.add_donor(Donor(response, [amount]))
     print(thank_letter(response, amount))
-    return
-
-
-def build_report():
-    header = ["Donor Name","Total Given", "Num Gifts","Average Gifts"]
-    report = ""
-    report += "-"*75
-    report += f"\n{header[0]:30}| {header[1]:15}| {header[2]:10}| {header[3]:20}\n"
-    report += "-"*75
-    for donor in sorted(donors, reverse = True):
-        report += f"\n{donor.name:30}${donor.total:>15,.2f}{donor.num_donation:>13}{donor.total/donor.num_donation:>15,.2f}"
-    report += "\n" + "-"*75
-    return report
 
 
 def report():
-    print(build_report())
+    print(db.build_report())
 
 
 def send_letters():
     file_path = "C:\_Python_210AC\Sp2018-Accelerated\students\WesWang\lesson9\\"
+    donors = db.donors
     for donor in donors:
-        letters = open(file_path + donor.name.replace(" ", "_") + "_Donation_Letter_" + str(date.today()) + ".txt", "w")
-        letters.write(thank_letter(donor.name, donor.donations[-1]))
+        letters = open(file_path + donors[donor].name.replace(" ", "_") + "_Donation_Letter_" + str(date.today()) + ".txt", "w")
+        letters.write(thank_letter(donors[donor].name, donors[donor].donations[-1]))
         letters.close()
     print("\nLetters to all donors' previous donations printed !!\n")
+
 
 def quit():
     print("You have successfully quit Mailroom")
@@ -160,5 +173,6 @@ def mainloop():
 
 
 if __name__ == "__main__":
+    db = DonorDB()
     default_donors()
     mainloop()
