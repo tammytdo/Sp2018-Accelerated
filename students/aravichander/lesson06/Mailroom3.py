5e9b3b9b1906fb14b3d81da6ba43ce4227620b21
Aarthi Ravichander <aravich@uw.edu>
2018-05-22 21:12:11 -0700

    Edited exceptions from Mailroom program

--- students/aravichander/lesson06/Mailroom3.py
+++ students/aravichander/lesson06/Mailroom3.py
@@ -1,11 +1,11 @@
-#Mailroom #2
+#Mailroom #3&4
 
 import sys
 import json
 import unittest 
 
 Donors = {"Bill Gates":[10000,12000],
-		  "Jeff Bezos":[50],`
+		  "Jeff Bezos":[50],
 		  "Mark Zuckerberg": [500,600,700],
 		  "Paul Allen": [250,350,450],
 		  "King of Siam": [200,250]
@@ -13,7 +13,7 @@ Donors = {"Bill Gates":[10000,12000],
 
 def printdonors():
 	for x,v in Donors.items():
-		print(x,"donated $",v)
+		print("{} donated ${}".format(x,v))
 
 def thankall():
 	for x in Donors:
@@ -30,32 +30,25 @@ def letterofthanks(donorname,totaldonated):
 	print("\nLetter of thanks has been saved to hard disk as text file under "+str(filename))
 
 def searchdonors(thankdonor):
-	#Not sure if these exceptions here are needed to be honest#
-	for x in Donors:
 		try:
-			if thankdonor == x:
-				totaldonated = sum(Donors[x])
-				letterofthanks(thankdonor,totaldonated)
+			print(Donors[thankdonor])
+			totaldonated = sum(Donors[thankdonor])
+			letterofthanks(thankdonor,totaldonated)
 		except KeyError:
-			print("That donor doesn't exist")
+			print("\nThat donor doesn't exist")
 
-#Needs to be modified to actually add something to original list
+
 def newdonation():
 	newdonor = input("Input name of new donor")
-	#modify to be able to take multiple donations
 	newdonations = input("Input donations, multiple if necessary")
 
 def thanks():
 	thankdonor = input("\nPlease enter full name of donor you'd like to thank or list to view donors\n")
-	#Added to catch name errors #
-	try:
-		if thankdonor == "list":
-			printdonors()
-			thanks()
-		else:
-			searchdonors(thankdonor)
-	except NameError:
-		print("That donor doesn't exist, try again")
+	if thankdonor == "list":
+		printdonors()
+		thanks()
+	else:
+		searchdonors(thankdonor)
 
 def report():
 	sorteddonors = sorted(Donors)
@@ -64,25 +57,22 @@ def report():
 
 def mainloop():
 	print("Introducing Mailroom!")
-	response = "0"
-	try:
-		while response < "4":
-			print("\nWhat do you want to do?")
-			print("\n(1) Send a thank you\n", "(2) Create a Report,\n", "(3) Send letters to everyone,\n","(4) Quit")
-			response = input(">> ")
-			print("response was,",response)
-			if response =="1":
-				print("\nYou have chosen the 'Thank' option")
-				thanks()
-			elif response == "2":
-				report()
-			elif response == "3":
-				thankall()
-			else:
-				print("\nSee you later!")
-				sys.exit
-	except ValueError:
-		print("Not an option, try again!")
+	done = False
+	while done is False:
+		print("\nWhat do you want to do?")
+		print("\n (1) Send a thank you\n", "(2) Create a Report,\n", "(3) Send letters to everyone,\n","(4) Quit")
+		response = input(">> ")
+		print("response was,",response)
+		if response =="1":
+			print("\nYou have chosen the 'Thank' option")
+			thanks()
+		elif response == "2":
+			report()
+		elif response == "3":
+			thankall()
+		else:
+			print("\nSee you later!")
+			done = True 
 
 
 
